d4b3730d737ef6a18551bb31f295e060f702748c
chaxs <echa.chaxs@gmail.com>
2026-02-07 19:49:14 +0700

    Update MainActivity.kt

--- app/src/main/java/com/chaxs/gcamconfig/MainActivity.kt
+++ app/src/main/java/com/chaxs/gcamconfig/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.chaxs.gcamconfig
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) 
+    {
         super.onCreate(savedInstanceState)
-        setContentView(android.R.layout.simple_list_item_1)
+        setContentView(R.layout.activity_main)
     }
 }
